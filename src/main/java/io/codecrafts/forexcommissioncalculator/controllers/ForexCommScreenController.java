418ab2e9a740ebce81dbd4f1102cf1af2c5d716d
Muhammad Zafar <xafarr@gmail.com>
2017-02-16 01:28:25 +1100

    Added NumberFormat to properly display numbers as currency.

--- src/main/java/io/codecrafts/forexcommissioncalculator/controllers/ForexCommScreenController.java
+++ src/main/java/io/codecrafts/forexcommissioncalculator/controllers/ForexCommScreenController.java
@@ -9,55 +9,60 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.math.RoundingMode;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ResourceBundle;
 
 public class ForexCommScreenController implements Initializable {
-    @FXML
-    private FloatingPointTextField spotPriceTextField;
-    @FXML
-    private FloatingPointTextField pipsTextField;
-    @FXML
-    private FloatingPointTextField commPercTextField;
-    @FXML
-    private FloatingPointTextField amountTextField;
-    @FXML
-    private Button buyButton, sellButton;
-    @FXML
-    private Label audTotalCustomerLabel, audTotalWholesaleLabel, difference;
-
-    public ForexCommScreenController() {
-    }
-
-    private EventHandler<ActionEvent> eventHandler = (event) -> {
-        Double spotPrice = Double.parseDouble(this.spotPriceTextField.getText());
-        Double pips = Double.parseDouble(this.pipsTextField.getText());
-        Double commPerc = Double.parseDouble(this.commPercTextField.getText());
-        Double amount = Double.parseDouble(this.amountTextField.getText());
-        ForexCommissionCalculator forexCommCalculator = new ForexCommissionCalculator(spotPrice, pips, commPerc, amount);
-        double totalAmountForCustomer;
-        double totalAmountForWholesaler;
-        double difference;
-        if (spotPrice > 0) {
-            if (event.getSource() == this.buyButton) {
-                totalAmountForCustomer = forexCommCalculator.getTotalAmountWhenBuyingFromCustomer();
-                totalAmountForWholesaler = forexCommCalculator.getTotalAmountWhenSellingToWholesaler();
-                difference = totalAmountForWholesaler - totalAmountForCustomer;
-            } else {
-                totalAmountForCustomer = forexCommCalculator.getTotalAmountWhenSellingToCustomer();
-                totalAmountForWholesaler = forexCommCalculator.getTotalAmountWhenBuyingFromWholesaler();
-                difference = totalAmountForCustomer - totalAmountForWholesaler;
-            }
-            this.audTotalCustomerLabel.setText(String.valueOf(totalAmountForCustomer));
-            this.audTotalWholesaleLabel.setText(String.valueOf(totalAmountForWholesaler));
-            this.difference.setText(String.valueOf(difference));
-
-        }
-    };
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        this.buyButton.setOnAction(eventHandler);
-        this.sellButton.setOnAction(eventHandler);
-    }
+	private final NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+	@FXML
+	private FloatingPointTextField spotPriceTextField;
+	@FXML
+	private FloatingPointTextField pipsTextField;
+	@FXML
+	private FloatingPointTextField commPercTextField;
+	@FXML
+	private FloatingPointTextField amountTextField;
+	@FXML
+	private Button buyButton, sellButton;
+	@FXML
+	private Label audTotalCustomerLabel, audTotalWholesaleLabel, difference;
+	
+	public ForexCommScreenController() {
+		numberFormat.setMaximumFractionDigits(2);
+		numberFormat.setRoundingMode(RoundingMode.HALF_UP);
+	}
+	
+	private EventHandler<ActionEvent> eventHandler = (event) -> {
+		Double spotPrice = Double.parseDouble(this.spotPriceTextField.getText());
+		Double pips = Double.parseDouble(this.pipsTextField.getText());
+		Double commPerc = Double.parseDouble(this.commPercTextField.getText());
+		Double amount = Double.parseDouble(this.amountTextField.getText());
+		ForexCommissionCalculator forexCommCalculator = new ForexCommissionCalculator(spotPrice, pips, commPerc, amount);
+		double totalAmountForCustomer;
+		double totalAmountForWholesaler;
+		double difference;
+		if (spotPrice > 0) {
+			if (event.getSource() == this.buyButton) {
+				totalAmountForCustomer = forexCommCalculator.getTotalAmountWhenBuyingFromCustomer();
+				totalAmountForWholesaler = forexCommCalculator.getTotalAmountWhenSellingToWholesaler();
+				difference = totalAmountForWholesaler - totalAmountForCustomer;
+			} else {
+				totalAmountForCustomer = forexCommCalculator.getTotalAmountWhenSellingToCustomer();
+				totalAmountForWholesaler = forexCommCalculator.getTotalAmountWhenBuyingFromWholesaler();
+				difference = totalAmountForCustomer - totalAmountForWholesaler;
+			}
+			this.audTotalCustomerLabel.setText(numberFormat.format(totalAmountForCustomer));
+			this.audTotalWholesaleLabel.setText(numberFormat.format(totalAmountForWholesaler));
+			this.difference.setText(numberFormat.format(difference));
+			
+		}
+	};
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.buyButton.setOnAction(eventHandler);
+		this.sellButton.setOnAction(eventHandler);
+	}
 }
